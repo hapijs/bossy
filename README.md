ca781e9f3ac6874bcf7905f494793be3ee17270c
Eran Hammer <eran@hueniverse.com>
2014-08-05 00:57:48 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-bossy
-=====
+#bossy
 
-Command line options parser
+Command line options parser.
+
+[![Build Status](https://secure.travis-ci.org/hapijs/bossy.png)](http://travis-ci.org/hapijs/bossy)
+
+Lead Maintainer - [Eran Hammer](https://github.com/hueniverse)
---
+++ test/index.js
@@ -0,0 +1,27 @@
+// Load modules
+
+var Lab = require('lab');
+var Bossy = require('../');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+
+describe('Bossy', function () {
+
+    it('parses command line', function (done) {
+
+        done();
+    });
+});
