2a5d9a598d92c949fcc7e6bffd1e72cd16d61094
Eran Hammer <eran@hueniverse.com>
2014-08-12 19:31:59 -0700

    baseline

--- test/index.js
+++ test/index.js
@@ -11,16 +11,78 @@ var internals = {};
 
 // Test shortcuts
 
+var lab = exports.lab = Lab.script();
+var before = lab.before;
+var after = lab.after;
+var describe = lab.experiment;
+var it = lab.test;
 var expect = Lab.expect;
-var before = Lab.before;
-var after = Lab.after;
-var describe = Lab.experiment;
-var it = Lab.test;
 
 
 describe('Bossy', function () {
 
+    var parse = function (line, definition, options) {
+
+        var orig = process.argv;
+        process.argv = [].concat('ignore', 'ignore', line.split(' '));
+        var result = null;
+        try {
+            result = Bossy.parse(definition, options);
+        }
+        catch (err) {
+            result = err;
+        }
+        process.argv = orig;
+        return result;
+    };
+
     it('parses command line', function (done) {
+
+        var line = '-a -cb --aa -C 1 -d x -d 2 -e 1-4 -f arg1 arg2 arg3';
+        var definition = {
+            a: {
+                type: 'boolean'
+            },
+            A: {
+                alias: 'aa',
+                type: 'boolean'
+            },
+            b: {
+                type: 'boolean'
+            },
+            c: {
+                type: 'boolean'
+            },
+            C: {
+                type: 'number'
+            },
+            d: {
+                type: 'string'
+            },
+            e: {
+                type: 'range'
+            },
+            f: {
+
+            },
+            g: {
+                type: 'boolean'
+            }
+        };
+
+        var argv = parse(line, definition);
+        expect(argv).to.not.be.instanceof(Error);
+        expect(argv).to.deep.equal({ a: true,
+            A: true,
+            b: true,
+            c: true,
+            g: false,
+            C: 1,
+            d: [ 'x', '2' ],
+            e: [1, 2, 3, 4],
+            f: 'arg1',
+            _: ['arg2', 'arg3']
+        });
 
         done();
     });
